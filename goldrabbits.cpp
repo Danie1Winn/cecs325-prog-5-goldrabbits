9b4cdfdef44c82b91c21d66ab74f403f4129dca1
Danie1Winn <daniel.winn.biz@gmail.com>
2025-11-18 11:46:15 -0800

    Initial file setup

---
+++ goldrabbits.cpp
@@ -0,0 +1,7 @@
+// Name: Daniel Winn
+// Class: CECS 325-02
+// Project Name: Prog 5 - Gold Rabbits
+// Due Date 11/18/25
+//
+// I certify that this program is my own original work. I did not copy any part of this program from
+// any other source. I further certify that I typed each and every line of code in this program.
